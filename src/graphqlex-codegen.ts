17a5509b6fe70e8471fd16cd61881ba2eac43e5f
tstewart <tstewart@klaudhaus.com>
2021-09-23 10:39:47 +1000

    First local working version

--- src/graphqlex-codegen.ts
+++ src/graphqlex-codegen.ts
@@ -1,9 +1,10 @@
 import { PluginFunction, Types } from "@graphql-codegen/plugin-helpers"
-import { concatAST, DocumentNode, FragmentDefinitionNode, GraphQLSchema, Kind, visit } from "graphql"
+import { concatAST, FragmentDefinitionNode, GraphQLSchema, Kind, OperationDefinitionNode, visit, print } from "graphql"
 import { ClientSideBaseVisitor, LoadedFragment } from "@graphql-codegen/visitor-plugin-common"
+import dedent from "ts-dedent"
 
 export const plugin: PluginFunction = (schema: GraphQLSchema, documents: Types.DocumentFile[], config: any) => {
-  const allAst = concatAST(documents.map(v => v.document as DocumentNode));
+  const allAst = concatAST(documents.map(v => v.document))
   const allFragments: LoadedFragment[] = [
     ...(allAst.definitions.filter(
       d => d.kind === Kind.FRAGMENT_DEFINITION
@@ -11,25 +12,90 @@ export const plugin: PluginFunction = (schema: GraphQLSchema, documents: Types.D
       node: fragmentDef,
       name: fragmentDef.name.value,
       onType: fragmentDef.typeCondition.name.value,
-      isExternal: false,
+      isExternal: false
     }))
-  ];
-  const visitor = new GraphqlexVisitor(schema, allFragments, config, {});
-  const visitorResult = visit(allAst, { leave: visitor });
+  ]
+  const visitor = new GraphqlexVisitor(schema, allFragments, config, {})
+  const visitorResult = visit(allAst, { leave: visitor })
+
+  const importsBlock = dedent`
+    import { Api, gql } from "graphqlex"
+    import {
+      ${visitor.typeImports.sort().join(",\n")}
+    } from "./graphql-types"
+    
+  `
+
+  const setApiBlock = dedent`
+    export let api: Api
+    export function setApi (toApi: Api) { api = toApi }
+    
+  `
 
   return {
-    prepend: visitor.getImports(),
     content: [
-      visitor.fragments,
-      ...visitorResult.definitions.filter((t: any) => typeof t === 'string'),
-      visitor.mainContent,
-    ].join('\n'),
-  };
-}
-
-class GraphqlexVisitor extends ClientSideBaseVisitor {
-  public get mainContent(): string {
-    return "Main content"
+      importsBlock,
+      setApiBlock,
+      ...visitorResult.definitions.filter((t: any) => typeof t === "string")
+    ].join("\n")
   }
 }
 
+class GraphqlexVisitor extends ClientSideBaseVisitor {
+  public get mainContent (): string {
+    return "Main content"
+  }
+
+  typeImports: string[] = []
+
+  OperationDefinition (node: OperationDefinitionNode): string {
+    this._collectedOperations.push(node)
+    const operationType = capitalise(node.operation)
+    const functionName = node.name.value + operationType
+    const operationTypeSuffix = this.getOperationSuffix(node, operationType)
+    const operationResultType = this.convertName(node, {
+      suffix: operationTypeSuffix + this._parsedConfig.operationResultSuffix
+    })
+    const operationVariablesType = this.convertName(node, {
+      suffix: operationTypeSuffix + "Variables"
+    })
+    this.typeImports.push(operationResultType, operationVariablesType)
+
+    let documentString = ""
+    const fragmentDocNames = this._transformFragments(node)
+    const fragments = this._fragments
+      .filter(f => fragmentDocNames.includes(this.getFragmentVariableName(f.name)))
+      .map(fragment => print(fragment.node))
+    const operationBlock = print(node)
+    const gqlBlock = [...fragments, operationBlock].join("\n")
+
+    if (node.operation === "query" || node.operation === "mutation") {
+      documentString = dedent`
+        export async function ${functionName} (vars: ${operationVariablesType}) {
+          const ${node.operation} = gql\`
+            ${gqlBlock}
+          \`
+          return <${operationResultType}> await api.run(${node.operation}, vars)
+        }
+
+      `
+    } else if (node.operation === "subscription") {
+      documentString = dedent`
+        export function ${functionName} (
+          handler?: (data: ${operationResultType}) => any,
+          vars: ${operationVariablesType} = {}
+        ) {
+          const subscription = gql\`
+            ${gqlBlock}
+          \`
+          api.subscribe(subscription, vars).onData(handler)
+        }
+        
+      `
+    }
+
+    return documentString
+  }
+}
+
+const capitalise = (str: String) => str.slice(0, 1).toUpperCase() + str.slice(1)
